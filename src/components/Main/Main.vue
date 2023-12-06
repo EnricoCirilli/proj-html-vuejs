60d2e791479076ef5f18507510f258d3fb82d7c8
EnricoCirilli <enrico.cirilli@gmail.com>
2023-12-06 09:53:36 +0100

    add: icon social nella sezione 2 del main

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -60,7 +60,9 @@
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
     </div>
     <div class="card-footer">
-      <small class="text-body-secondary">Last updated 3 mins ago</small>
+      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-facebook"></i></small>
+      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-x-twitter"></i></small>
+      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-instagram"></i></small>
     </div>
   </div>
   <div class="card">
@@ -70,7 +72,9 @@
       <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
     </div>
     <div class="card-footer">
-      <small class="text-body-secondary">Last updated 3 mins ago</small>
+      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-facebook"></i></small>
+      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-x-twitter"></i></small>
+      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-instagram"></i></small>
     </div>
   </div>
   <div class="card">
@@ -80,7 +84,9 @@
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
     </div>
     <div class="card-footer">
-      <small class="text-body-secondary">Last updated 3 mins ago</small>
+      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-facebook"></i></small>
+      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-x-twitter"></i></small>
+      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-instagram"></i></small>
     </div>
   </div>
 </div>
