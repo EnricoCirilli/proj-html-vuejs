44dbf007df92a961bd04d8b80a022f4d89f823e3
EnricoCirilli <enrico.cirilli@gmail.com>
2023-12-06 11:27:54 +0100

    add: struttura sezioni  in costruzione

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -8,108 +8,277 @@
   <!-- section 1 -->
   <section class="section-1">
     <h1 class="text-center">Welcome To Avada Health</h1>
-    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt maiores nam dolore vel molestiae quasi reiciendis nemo repellendus nisi accusamus.</p>
+    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt maiores nam dolore vel
+      molestiae quasi reiciendis nemo repellendus nisi accusamus.</p>
     <div class="card-group">
-  <div class="card">
-    <img src="/src/assets/images/icon-1.png" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">SAME DAY APPOINMENT</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      <div class="card border-light none">
+        <img src="/src/assets/images/icon-1.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">SAME DAY APPOINMENT</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+            This content is a little bit longer.</p>
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+      <div class="card border-light none">
+        <img src="/src/assets/images/icon-2.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">WORLD CLASS FACILITIES</h5>
+          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+      <div class="card border-light none">
+        <img src="/src/assets/images/icon-3.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+            This card has even longer content than the first to show that equal height action.</p>
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+      <div class="card border-light none">
+        <img src="/src/assets/images/icon-4.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+            This card has even longer content than the first to show that equal height action.</p>
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="card">
-    <img src="/src/assets/images/icon-2.png" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">WORLD CLASS FACILITIES</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="/src/assets/images/icon-3.png" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="/src/assets/images/icon-4.png" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-</div>
   </section>
   <!-- section 2 -->
   <div class="text-center">
-  <img src="/src/assets/images/icon-5.png" alt="">
-  <h2 >MEET OUR DOCTOR</h2>
-  <hr>
-  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique quia necessitatibus iste voluptatum excepturi dolorem laudantium ab aperiam ipsam nesciunt.</p>
+    <img src="/src/assets/images/icon-5.png" alt="">
+    <h2>MEET OUR DOCTOR</h2>
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique quia necessitatibus iste voluptatum excepturi
+      dolorem laudantium ab aperiam ipsam nesciunt.</p>
   </div>
   <section class="section-2">
     <div class="card-group">
-  <div class="card">
-    <img src="/src/assets/images/doctor-1-200x206.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <div class="card">
+        <img src="/src/assets/images/doctor-1-200x206.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+            This content is a little bit longer.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-body-secondary m-2"><i class="fa-brands fa-square-facebook"></i></small>
+          <small class="text-body-secondary m-2"><i class="fa-brands fa-square-x-twitter"></i></small>
+          <small class="text-body-secondary m-2"><i class="fa-brands fa-square-instagram"></i></small>
+        </div>
+      </div>
+      <div class="card">
+        <img src="/src/assets/images/doctor-2-200x206.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-body-secondary m-2"><i class="fa-brands fa-square-facebook"></i></small>
+          <small class="text-body-secondary m-2"><i class="fa-brands fa-square-x-twitter"></i></small>
+          <small class="text-body-secondary m-2"><i class="fa-brands fa-square-instagram"></i></small>
+        </div>
+      </div>
+      <div class="card">
+        <img src="/src/assets/images/doctor-3-200x206.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+            This card has even longer content than the first to show that equal height action.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-body-secondary m-2"><i class="fa-brands fa-square-facebook"></i></small>
+          <small class="text-body-secondary m-2"><i class="fa-brands fa-square-x-twitter"></i></small>
+          <small class="text-body-secondary m-2"><i class="fa-brands fa-square-instagram"></i></small>
+        </div>
+      </div>
     </div>
-    <div class="card-footer">
-      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-facebook"></i></small>
-      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-x-twitter"></i></small>
-      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-instagram"></i></small>
+  </section>
+
+  <!-- section 3 -->
+  <section>
+    <div class="text-center">
+      <img src="/src/assets/images/icon-6.png" alt="">
+      <h2>OUR HEALTH SERVICES</h2>
+      <hr>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique quia necessitatibus iste voluptatum excepturi
+        dolorem laudantium ab aperiam ipsam nesciunt.</p>
+    </div>
+    <div class="card-group">
+      <div class="card border border-0">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+            This content is a little bit longer.</p>
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+      <div class="card border border-0">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+      <div class="card border border-0">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+            This card has even longer content than the first to show that equal height action.</p>
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+    </div>
+    <div class="card-group">
+      <div class="card border border-0">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+            This content is a little bit longer.</p>
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+      <div class="card border border-0">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+      <div class="card border border-0">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+            This card has even longer content than the first to show that equal height action.</p>
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+    </div>
+
+  </section>
+<!-- SECTION 4 -->
+  <section>
+    <div class="section-4">
+      <div class="text-center">
+      <img src="/src/assets/images/icon-6.png" alt="">
+      <h2>OUR HEALTH SERVICES</h2>
+      <hr>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique quia necessitatibus iste voluptatum excepturi
+        dolorem laudantium ab aperiam ipsam nesciunt.</p>
+        <img src="/src/assets/images/icon-6.png" alt="">
+    </div>
+    </div>
+  </section>
+<!-- SECTION 5 -->
+  <section>
+    <div class="row">
+       <div class="col-sm-4">
+         <div class="card">
+           <div class="card-body">
+             <h5 class="card-title">Special title treatment</h5>
+             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+             <a href="#" class="btn btn-primary">Go somewhere</a>
+           </div>
+         </div>
+       </div>
+       <div class="col-sm-4">
+         <div class="card">
+           <div class="card-body">
+             <h5 class="card-title">Special title treatment</h5>
+             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+             <a href="#" class="btn btn-primary">Go somewhere</a>
+           </div>
+         </div>
+       </div>
+       <div class="col-sm-4">
+         <div class="card">
+           <div class="card-body">
+             <h5 class="card-title">Special title treatment</h5>
+             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+             <a href="#" class="btn btn-primary">Go somewhere</a>
+           </div>
+         </div>
+       </div>
+    </div>
+
+  </section>
+<!-- SECTION 6 -->
+  <section class="section-6">
+    <h1 class="text-center">Welcome To Avada Health</h1>
+    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt maiores nam dolore vel
+      molestiae quasi reiciendis nemo repellendus nisi accusamus.</p>
+      
+      <div class="row g-2 mx-auto" style="width:650px">
+  <div class="col-md m-4">
+    <div class="form-floating">
+      <input type="email" class="form-control" style="background-color: transparent;" id="floatingInputGrid" placeholder="name@example.com" value="mdo@example.com">
+      <label for="floatingInputGrid">Email address</label>
     </div>
   </div>
-  <div class="card">
-    <img src="/src/assets/images/doctor-2-200x206.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-facebook"></i></small>
-      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-x-twitter"></i></small>
-      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-instagram"></i></small>
+  <div class="col-md m-4">
+    <div class="form-floating">
+      <input type="email" class="form-control" style="background-color: transparent;" id="floatingInputGrid" placeholder="name@example.com" value="mdo@example.com">
+      <label for="floatingInputGrid">Email address</label>
     </div>
   </div>
-  <div class="card">
-    <img src="/src/assets/images/doctor-3-200x206.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-facebook"></i></small>
-      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-x-twitter"></i></small>
-      <small class="text-body-secondary m-2"><i class="fa-brands fa-square-instagram"></i></small>
+  </div>
+
+  <div class="row g-2 mx-auto" style="width:650px">
+  <div class="col-md m-4">
+    <div class="form-floating">
+      <input type="email" class="form-control" style="background-color: transparent;" id="floatingInputGrid" placeholder="name@example.com" value="mdo@example.com">
+      <label for="floatingInputGrid">Email address</label>
     </div>
   </div>
+  <div class="col-md m-4">
+    <div class="form-floating">
+      <input type="email" class="form-control"  style="background-color: transparent;" id="floatingInputGrid" placeholder="name@example.com" value="mdo@example.com">
+      <label for="floatingInputGrid">Email address</label>
+    </div>
+  </div>
+  </div>
+
+<div class="mx-auto p-2" style="width: 600px;">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" style="background-color: transparent;" id="exampleFormControlTextarea1" rows="3"></textarea>
 </div>
   </section>
-  <section>
-    3
-  </section>
-  
-  <section>
-    4
-  </section>
-  
-  <section>
-    5
-  </section>
-  
-  <section>
-    6
-  </section>
-
 </template>
 
-<style>
+<style lang="scss" scoped>// General style
+.none {
+  border: none;
+}
+
+img {
+  width: 100px;
+  margin: 0 auto;
+}
+.section-4{
+  background-image:   url("src/assets/images/paralax-bg-tour-facilities.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  border: 2px solid red;
+  height: 500px;
+}
+.section-6{
+  background-image: url("src/assets/images/make-an-appointment.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  border: 2px solid red;
+  height: 500px;
+}
 
 
 </style>
+
